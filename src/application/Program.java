3b5628372e846e11d2738e012bbf12ea012a5dda
Leonardo Zwierzykowski <leonardozw46@gmail.com>
2021-05-03 21:59:16 -0300

    Starting to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,12 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 	}
 
 }
